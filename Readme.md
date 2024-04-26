a1b09f84dd0d901310fd6117e617ca4e89cf6a63
Etienne Bagnoud <etienne@artisan-numerique.ch>
2024-04-26 11:08:19 +0200

    Documentation, typing and additional options.

--- Readme.md
+++ Readme.md
@@ -1,8 +1,13 @@
 # Snowflake53
 
-A trait to generate 53 bits snowflake ID. ID fit into js Number.MAX_SAFE_INTEGER.
+A trait to generate 53 bits snowflake id to fit into js Number.MAX_SAFE_INTEGER.
 Sequence counting is done with shared memory and machine ID comes from
-environment variable.
+environment variable or parameters
+It also possible to generate 64 bits snowflake id.
+
+It dosen't require any external infrastructure (like Memcached or Redis or ...)
+to keep sequence counting as it is done by shared memory and use semaphore for
+locking. So other processes can tap into the sequence counting too :)
 
 ## Usage
 
@@ -16,6 +21,86 @@ class IDGen {
 }
 
 $gen = new IDGen();
-echo $gen->generateId() . PHP_EOL;
+echo $gen->get53() . PHP_EOL;
+echo $gen->get64() . PHP_EOL;
 
 ```
+
+All functions are declared as static, so it is possible to use without a class
+instance.
+
+```php
+require_once 'vendor/autoload.php';
+
+use Snowflake53\ID;
+
+class IDGen {
+    use ID;
+}
+
+echo IDGen::get53() . PHP_EOL;
+echo IDGen::get64() . PHP_EOL;
+```
+
+You can destroy, if you choose to, shared memory semgent and semaphore by calling
+`destroySHM`:
+
+```php
+require_once 'vendor/autoload.php';
+
+use Snowflake53\ID;
+
+class IDGen {
+    use ID;
+}
+
+IDGen::destroySHM(); 
+```
+
+## Shared memory path
+
+Shared memory use a file path as identity. By default it is `__FILE__`.  But if
+you use this in several different places on your server but still want all ID
+to be in sequence, you can set the public static variable $SHMPath to the path
+you want. The file __must__ exist.
+
+```php
+require_once 'src/Snowflake53.php';
+
+use Snowflake53\ID;
+
+class IDGen {
+    use ID;
+}
+
+IDGen::$SHMPath = '/tmp/snowflake53';
+echo IDGen::get53() . PHP_EOL;
+echo IDGen::get64() . PHP_EOL;
+
+
+$gen = new IDGen();
+$gen::$SHMPath = '/tmp/snowflake53';
+echo $gen->get53() . PHP_EOL;
+echo $gen->get64() . PHP_EOL;
+```
+
+## Machine ID
+
+Machine ID can be passed as argument of `get53` or `get64`. If not, it will try to
+get the ID from environment variable `SNOWFLAK53_MACHINE_ID`, 
+`SNOWFLAKE64_MACHINE_ID` or `SNOWFLAKE_MACHINE_ID`. 
+When getting a 53 bits ID, `SNOWFLAK53_MACHINE_ID` will be look at and then 
+`SNOWFLAKE_MACHINE_ID`, when getting a 64 bits ID it's the opposite (first
+`SNOWFLAKE64_MACHINE_ID` and then `SNOWFLAKE_MACHINE_ID`).
+
+The idea behind that is you set a unique machine  for 64 and 53 bits and so, you
+use `SNOWFLAKE_MACHINE_ID` or you have a different machine ID for 53 bits or 64
+bits and so you set `SNOWFLAKE53_MACHINE_ID` and `SNOWFLAKE64_MACHINE_ID`.
+
+## License
+
+Under [MIT license](https://opensource.org/license/mit).
+
+## Developer
+
+ * Etienne Bagnoud <etienne@artnum.ch>
--- src/Snowflake53.php
+++ src/Snowflake53.php
@@ -3,6 +3,8 @@
 namespace Snowflake53;
 
 use Exception;
+use SysvSharedMemory;
+use SysvSemaphore;
 
   /* 10th of seconds since 1st march 2024 at 9:00:00 */
   CONST EPOCH53 =   17092800000;
@@ -23,8 +25,15 @@ use Exception;
  * so it doesn't need to have Redis, Memcached or any other shared storage.
  */
 trait ID {
-    private static function initSHM ($prj = PRJ53) {
-        $shmId = ftok(__FILE__, $prj);
+    public static string $SHMPath = __FILE__;
+    /**
+     * Initialize shared memory and semaphore. It is used to count sequences.
+     * @param string $prj Either '5' or '6'.
+     * @return (SysvSharedMemory|SysvSemaphore)[] Semaphore and shared memory segment.
+     * @throws Exception 
+     */
+    private static function initSHM (string $prj = PRJ53):array {
+        $shmId = ftok(self::$SHMPath, $prj);
         if ($shmId === -1) {
             throw new Exception('Error creating shared memory segment');
         }
@@ -54,7 +63,24 @@ trait ID {
         return [$shm, $sem];
     }
 
-    private static function getSequenceId ($shm, $seq = SEQ53) {
+    /**
+     * Destroy shared memory and semaphore.
+     * @return void 
+     * @throws Exception 
+     */
+    public static function destroySHM ():void {
+        [$shm, $sem] = self::initSHM(PRJ53);
+        sem_remove($sem);
+        shm_remove($shm);
+    }
+
+    /**
+     * Get sequence ID from shared memory.
+     * @param mixed $shm Shared memory handle.
+     * @param int $seq Either SEQ53 or SEQ64.
+     * @return mixed 
+     */
+    private static function getSequenceId (SysvSharedMemory $shm, int $seq = SEQ53):int {
         $sequenceId = 0;
         if (shm_has_var($shm, 0)) {
             $sequenceId = shm_get_var($shm, 0);
@@ -65,18 +91,18 @@ trait ID {
         return $sequenceId;
     }
 
-    private static function updateSequenceId ($shm, $sequenceId) {
+    private static function updateSequenceId (SysvSharedMemory $shm, int $sequenceId):void {
         if (!shm_put_var($shm, 0, $sequenceId + 1)) {
             throw new Exception('Error updating sequence ID');
         }
     }
 
-    private static function releaseSHM ($shm, $sem) {
+    private static function releaseSHM (SysvSharedMemory $shm, $sem):void {
         sem_release($sem);
         shm_detach($shm);
     }
 
-    private static function getLastTime ($shm) {
+    private static function getLastTime (SysvSharedMemory $shm):int {
         $lastTime = 0;
         if (shm_has_var($shm, 1)) {
             $lastTime = shm_get_var($shm, 1);
@@ -84,24 +110,31 @@ trait ID {
         return $lastTime;
     }
 
-    private static function updateLastTime ($shm, $time) {
+    private static function updateLastTime (SysvSharedMemory $shm, int $time):void {
         if (!shm_put_var($shm, 1, $time)) {
             throw new Exception('Error updating last time');
         }
     }
 
-    private static function getMachineId ($mask = MACHINE53) {
+    /**
+     * Get machine ID from environment variables.
+     * For 53 bits variant, it will look, in order, for SNOWFLAKE53_MACHINE_ID
+     * and SNOWFLAKE_MACHINE_ID. For 64 bits variant, it will look for 
+     * SNOWFLAKE64_MACHINE_ID and SNOWFLAKE_MACHINE_ID.
+     * So machine ID can be set differently for 53 and 64 bits variants.
+     * @param int $mask Either MACHINE53 or MACHINE64
+     * @return int masked, by $mask, integer.
+     */
+    private static function getMachineId (int $mask = MACHINE53):int {
         $machineId = 0;
         switch ($mask) {
             default:
             case MACHINE53:
                 $machineId = getenv('SNOWFLAK53_MACHINE_ID')
-                    || getenv('SNOWFLAKE64_MACHINE_ID')
                     || getenv('SNOWFLAKE_MACHINE_ID');
                 break;
             case MACHINE64:
                 $machineId = getenv('SNOWFLAK64_MACHINE_ID')
-                    || getenv('SNOWFLAKE53_MACHINE_ID')
                     || getenv('SNOWFLAKE_MACHINE_ID');
                 break;
         }
@@ -111,21 +144,42 @@ trait ID {
         return intval($machineId) & $mask;
     }
 
-    private static function mixValues53 ($time, $machineId, $sequenceId) {
+    /**
+     * Mix values to generate a 53 bits ID.
+     * @param int $time The time
+     * @param int $machineId The machine
+     * @param int $sequenceId The sequence
+     * @return int The ID
+     */
+    private static function mixValues53 (int $time, int $machineId, int $sequenceId):int {
         return 0 
             | ($time << 18) 
             | (($machineId & MACHINE53) << 10)  
             | ($sequenceId & SEQ53);
     }
 
-    private static function mixValues64 ($time, $machineId, $sequenceId) {
+    /**
+     * Mix values to generate a 64 bits ID.
+     * @param mixed $time The time
+     * @param mixed $machineId The machine
+     * @param mixed $sequenceId The sequence
+     * @return int The ID
+     */
+    private static function mixValues64 (int $time, int $machineId, int $sequenceId):int {
         return 0 
             | ($time << 23) 
             | (($machineId & MACHINE64) << 13)  
             | ($sequenceId & SEQ64);
     }
 
-    public static function generateId(int $machineId = -1):int {
+    /**
+     * Generate a 53 bits ID.
+     * @param int $machineId The machine ID, if set to -1, it will look for SNOWFLAKE53_MACHINE_ID, SNOWFLAKE64_MACHINE_ID
+     * and SNOWFLAKE_MACHINE_ID environment variables. If not found, it will use 0.
+     * @return int A 53 bits integer.
+     * @throws Exception 
+     */
+    public static function get53(int $machineId = -1):int {
         try {
             $time = (intval(microtime(true) * 10) - EPOCH53) & 0x3FFFFFFFFFF;
             
@@ -154,11 +208,23 @@ trait ID {
         }
     }
 
-    public static function get53 (int $machineId = -1):int {
-        return self::generateId($machineId);
+
+    /**
+     * @see get53
+     * @deprecated 
+     */
+    public static function generateId (int $machineId = -1):int {
+        return self::get53($machineId);
     }
 
-    public static function generateId64(int $machineId = -1):int {
+    /**
+     * Generate a 64 bits ID.
+     * @param int $machineId The machine ID, if set to -1, it will look for SNOWFLAKE64_MACHINE_ID, SNOWFLAKE53_MACHINE_ID
+     * and SNOWFLAKE_MACHINE_ID environment variables. If not found, it will use 0.
+     * @return int A 64 bits integer.
+     * @throws Exception 
+     */
+    public static function get64(int $machineId = -1):int {
         try {
             $time = (intval(microtime(true) * 1000) - EPOCH64) & 0x7FFFFFFFF;
                         
@@ -185,9 +251,13 @@ trait ID {
         } catch (Exception $e) {
             throw new Exception('Error generating ID', 0, $e);
         }
-    }   
+    }
 
-    public static function get64 (int $machineId = -1):int {
-        return self::generateId64($machineId);
+    /**
+     * @see get64
+     * @deprecated 
+     */
+    public static function generateId64 (int $machineId = -1):int {
+        return self::get64($machineId);
     }
 }
